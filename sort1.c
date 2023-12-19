6e1794eeae2cfdf22ae9ad97b21400dd57908830
sp35000 <sp35000@yahoo.com.br>
2023-12-19 15:36:07 -0500

    feat: comparing selection sort, bubble sort, merge sort

--- sort1.c
+++ sort1.c
@@ -23,11 +23,11 @@ int main(void)
       n[x] = n[min];
       n[min] = tmp;
     }
-    for (int i = 0; i < (size-1); i++) {
-      printf("%i ",n[i]);
-      // draw(n[i]);
-    }
-    printf("\n");
+    // for (int i = 0; i < (size-1); i++) {
+    //   printf("%i ",n[i]);
+    //   // draw(n[i]);
+    // }
+    // printf("\n");
 }
 
 void draw(int n)
--- sort2.c
+++ sort2.c
@@ -20,11 +20,11 @@ int main(void)
         }
       }
     }
-    for (int i = 0; i < (size-1); i++) {
-      printf("%i ",n[i]);
-      // draw(n[i]);
-    }
-    printf("\n");
+    // for (int i = 0; i < (size-1); i++) {
+    //   printf("%i ",n[i]);
+    //   // draw(n[i]);
+    // }
+    // printf("\n");
 }
 
 void draw(int n)
